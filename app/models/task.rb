e90397250b3c9b48bc3c9a1c5824f26b002b9c89
ririka nishiuji <rosefur@ririkanoMacBook-Air.local>
2026-03-26 20:02:26 +0900

    Taskモデルのバリデーションとリレーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, class_name: "User", foreign_key: "user_id"
+
+  belongs_to :assignee, class_name: "User", foreign_key: "assignee_id", optional: true
+
+  has_many :sub_tasks, dependent: :destroy
+
+  validates :title, presence: true
+  validates :status, presence: true
+  validates :priority, presence: true
 end
