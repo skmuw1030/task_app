7ed0f29b67735045a3eda1d0dfa82e30fe6a00a3
ririka nishiuji <rosefur@ririkanoMacBook-Air.local>
2026-03-26 21:52:22 +0900

    Userモデルのリレーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :created_tasks, class_name: "Task", foreign_key: "user_id", dependent: :destroy
+  has_many :assigned_tasks, class_name: "Task", foreign_key: "assignee_id"
+  has_many :sub_tasks, through: :created_tasks
+
   validates :name, presence: true
 end
