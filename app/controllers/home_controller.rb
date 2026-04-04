c9679d75278599bf97a844489c813e389a0ac783
ririka nishiuji <rosefur@ririkanoMacBook-Air.local>
2026-04-04 14:18:05 +0900

    カンバンボードの実装
    -TailwindCSSを使ってカンバンボードを作成。
    -担当者を選べるようにして、担当者AさんのボードにAさんのタスクが表示されるようにした。
    -タスク新規作成時にはシンプルに、編集時は不足書類についても入力できるようにした。
    -タスク担当者を作成者以外が変えられないようにした。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       @tasks = current_user.assigned_tasks.includes(:creator)
+
+      @todo_tasks =@tasks.where(status: "未着手")
+      @doing_tasks =@tasks.where(status: "進行中")
+      @done_tasks =@tasks.where(status: "完了")
     end
   end
 end
