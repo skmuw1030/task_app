c91aac1975fdb619a3deda6cda145085ea2c00a0
ririka nishiuji <rosefur@ririkanoMacBook-Air.local>
2026-04-12 22:03:39 +0900

    認証画面の見た目調整とアカウント削除モーダルの実装
    -新規登録画面、ログイン画面、パスワード忘れてしまった場合の画面、アカウント編集画面をtailwind cssで見た目を整えた。
    -アカウント削除する場合はモーダル表示されるよう実装
    -アカウント削除した場合、担当分のタスクの担当者は空になるようモデルで設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :created_tasks, class_name: "Task", foreign_key: "user_id", dependent: :destroy
-  has_many :assigned_tasks, class_name: "Task", foreign_key: "assignee_id"
+  has_many :assigned_tasks, class_name: "Task", foreign_key: "assignee_id", dependent: :nullify
   has_many :sub_tasks, through: :created_tasks
 
   validates :name, presence: true
   validates :password, format: {
     with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
-    message: "は英字と数字の両方を含めて設定してください"
+    message: "は英字と数字の両方を含めて8文字以上で設定してください"
   }, if: :password_required?
 
   private
