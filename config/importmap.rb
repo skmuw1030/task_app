b70d0fa31d773d1379092aca611747be995fe038
ririka nishiuji <rosefur@ririkanoMacBook-Air.local>
2026-04-27 00:10:34 +0900

    importmapとjsのパスを同じに

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "sortablejs", to: "https://cdn.jsdelivr.net/npm/sortablejs@latest/modular/sortable.esm.js"
+pin "sortablejs", to: "sortable.js"
 pin "@rails/request.js", to: "request.js"
