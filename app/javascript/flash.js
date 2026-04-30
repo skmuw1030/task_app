14111c5cc5af86d4d517a70eced66d2ac3437bd9
ririka nishiuji <rosefur@ririkanoMacBook-Air.local>
2026-04-30 21:03:47 +0900

    フラッシュメッセージを自動で消えるよう調整

--- app/javascript/flash.js
+++ app/javascript/flash.js
@@ -1,10 +1,11 @@
-console.log("flash loaded");
 document.addEventListener("turbo:load", () => {
   const flash = document.querySelector(".flash-message");
-  console.log("flash loaded");
-
+  
   if (flash) {
     console.log("found flash");
-    flash.remove();
+
+    setTimeout(() => {
+      flash.remove();
+    }, 500);
   }
 });
